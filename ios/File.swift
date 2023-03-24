1cd96e69dec2dc46d774e643e13641479931fc8a
wangyinhao <deep.wang@withinspace.com>
2023-03-24 17:37:23 +0800

    request文件配置

---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  SpaceApp
+//
+//  Created by Space on 2023/3/23.
+//
+
+import Foundation
---
+++ src/utils/request/api/api.ts
@@ -0,0 +1,6 @@
+const ApiConfig = {
+    // 接口注释
+    apiName: "接口地址",
+}
+
+export default ApiConfig;
